2a9a2fab52b1319f5a82012bdc3c09f48a77c100
Santiago Pavon <santiago@dit.upm.es>
2017-03-23 14:16:25 +0100

    Tema 9 - DDBB, Sequelize y SQLite.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,38 +1,43 @@
 var models = require("../models");
+var Sequelize = require('sequelize');
 
 
 // Autoload el quiz asociado a :quizId
 exports.load = function (req, res, next, quizId) {
 
-    var quiz = models.Quiz.findById(Number(quizId));
-
-    if (quiz) {
-        req.quiz = quiz;
-        next();
-    } else {
-        throw new Error('No existe ningún quiz con id=' + quizId);
-    }
+    models.Quiz.findById(quizId)
+    .then(function (quiz) {
+        if (quiz) {
+            req.quiz = quiz;
+            next();
+        } else {
+            throw new Error('No existe ningún quiz con id=' + quizId);
+        }
+    })
+    .catch(function (error) {
+        next(error);
+    });
 };
 
 
 // GET /quizzes
 exports.index = function (req, res, next) {
 
-    var quizzes = models.Quiz.findAll();
-
-    res.render('quizzes/index.ejs', {quizzes: quizzes});
+    models.Quiz.findAll()
+    .then(function (quizzes) {
+        res.render('quizzes/index.ejs', {quizzes: quizzes});
+    })
+    .catch(function (error) {
+        next(error);
+    });
 };
 
 
 // GET /quizzes/:quizId
 exports.show = function (req, res, next) {
 
-    var answer = req.query.answer || '';
-
     res.render('quizzes/show', {quiz: req.quiz});
 };
-
-
 
 
 // GET /quizzes/new
@@ -43,24 +48,32 @@ exports.new = function (req, res, next) {
     res.render('quizzes/new', {quiz: quiz});
 };
 
+
 // POST /quizzes/create
 exports.create = function (req, res, next) {
 
-    var quiz = {
+    var quiz = models.Quiz.build({
         question: req.body.question,
         answer: req.body.answer
-    };
+    });
 
-    // Validar que no estan vacios
-    if (!quiz.question || !quiz.answer) {
+    // guarda en DB los campos pregunta y respuesta de quiz
+    quiz.save({fields: ["question", "answer"]})
+    .then(function (quiz) {
+        res.redirect('/quizzes/' + quiz.id);
+    })
+    .catch(Sequelize.ValidationError, function (error) {
+
+        console.log('Errores en el formulario:');
+        for (var i in error.errors) {
+            console.log(error.errors[i].value);
+        }
+
         res.render('quizzes/new', {quiz: quiz});
-        return;
-    }
-
-    // guarda el nuevo quiz
-    quiz = models.Quiz.create(quiz);
-
-    res.redirect('/quizzes/' + quiz.id);
+    })
+    .catch(function (error) {
+        next(error);
+    });
 };
 
 
@@ -77,18 +90,35 @@ exports.update = function (req, res, next) {
     req.quiz.question = req.body.question;
     req.quiz.answer = req.body.answer;
 
-    models.Quiz.update(req.quiz);
+    req.quiz.save({fields: ["question", "answer"]})
+    .then(function (quiz) {
+        res.redirect('/quizzes/' + req.quiz.id);
+    })
+    .catch(Sequelize.ValidationError, function (error) {
 
-    res.redirect('/quizzes/' + req.quiz.id);
+        console.log('Errores en el formulario:');
+        for (var i in error.errors) {
+            console.log(error.errors[i].value);
+        }
+
+        res.render('quizzes/edit', {quiz: req.quiz});
+    })
+    .catch(function (error) {
+        next(error);
+    });
 };
 
 
 // DELETE /quizzes/:quizId
 exports.destroy = function (req, res, next) {
 
-    models.Quiz.destroy(req.quiz);
-
-    res.redirect('/quizzes');
+    req.quiz.destroy()
+    .then(function () {
+        res.redirect('/quizzes');
+    })
+    .catch(function (error) {
+        next(error);
+    });
 };
 
 
--- models/index.js
+++ models/index.js
@@ -1,7 +1,24 @@
+var path = require('path');
+
+// Cargar ORM
+var Sequelize = require('sequelize');
+
+// Usando BBDD SQLite:
+var sequelize = new Sequelize("sqlite:///", {storage: "quiz.sqlite"});
+
+// Importar la definicion de la tabla Quiz de quiz.js
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
 
-var Quiz = require("./quiz");
+// Crear tablas
+sequelize.sync()
+.then(function () {
+    console.log('Bases de datos creadas con éxito');
+})
+.catch(function (error) {
+    console.log("Error creando las tablas de la BBDD:", error);
+    process.exit(1);
+});
 
 
-exports.Quiz = Quiz;
-
+exports.Quiz = Quiz; // exportar definición de tabla Quiz
