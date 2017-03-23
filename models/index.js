23dcbccdf6f7beff5f91e2714577a4e9bee67f9b
Santiago Pavon <santiago@dit.upm.es>
2017-03-23 21:15:59 +0100

    Tema 11 - Heroku Postgres.

--- models/index.js
+++ models/index.js
@@ -3,12 +3,28 @@ var path = require('path');
 // Cargar ORM
 var Sequelize = require('sequelize');
 
-// Usando BBDD SQLite:
-var sequelize = new Sequelize("sqlite:///", {storage: "quiz.sqlite"});
+// Para usar en local BBDD SQLite:
+//    DATABASE_URL = sqlite:///
+//    DATABASE_STORAGE = quiz.sqlite
+// Para usar en Heroku BBDD Postgres:
+//    DATABASE_URL = postgres://user:passwd@host:port/database
+
+var url, storage;
+
+if (!process.env.DATABASE_URL) {
+    url = "sqlite:///";
+    storage = "quiz.sqlite";
+} else {
+    url = process.env.DATABASE_URL;
+    storage = process.env.DATABASE_STORAGE || "";
+}
+
+var sequelize = new Sequelize(url, {storage: storage});
+
+
 
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
-
 
 // Crear tablas
 sequelize.sync()
