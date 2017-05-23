ac5c7ba023f42a05f0edca8ff42a32e86000ea99
root <root@DESKTOP-5QJ609J.localdomain>
2017-05-23 19:58:51 +0200

    practica52

--- models/index.js
+++ models/index.js
@@ -25,6 +25,12 @@ var sequelize = new Sequelize(url, {storage: storage});
 
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
-
+sequilyze.sync()
+.then(function(){
+	console.log("creados");
+})
+.catch(function(error){
+	console.log("Error");
+});
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
