b5dec0ee63a8efe6628288c4df56f275f6252bbd
root <root@DESKTOP-5QJ609J.localdomain>
2017-05-23 20:00:00 +0200

    practica52

--- models/index.js
+++ models/index.js
@@ -25,7 +25,7 @@ var sequelize = new Sequelize(url, {storage: storage});
 
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
-sequilyze.sync()
+sequelize.sync()
 .then(function(){
 	console.log("creados");
 })
