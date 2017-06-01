2f167bcae300ff9f2de6f4c4cc0a3daff2756735
root <root@DESKTOP-5QJ609J.localdomain>
2017-06-01 19:13:39 +0200

    ultima version

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -193,6 +193,7 @@ exports.randomplay = function (req, res, next) {
     if (!req.session.score) req.session.score = 0;
     if (!req.session.questions) req.session.questions = [-1];
 
+
     models.Quiz.count()
     .then(function(count) {
 
@@ -208,9 +209,13 @@ exports.randomplay = function (req, res, next) {
             var random = parseInt(Math.random() * quizzes.length);
             quizID = quizzes[random].id;
         } else {
+        	req.session.questions=[-1];
+        	var score1=req.session.score;
+        	req.session.score=0;
             res.render('quizzes/randomnomore', {
-                score: req.session.score
+                score: score1
             });
+          
         }
 
         return models.Quiz.findById(quizID);
@@ -237,16 +242,16 @@ exports.randomcheck = function (req, res, next) {
     var answer = req.query.answer || "";
 
     var result = answer.toLowerCase().trim() === req.quiz.answer.toLowerCase().trim();
-
+    var score=req.session.score;
     if (result) {
         req.session.score++;
-        
-  
-    }
-    else{req.session.score=0;}
+       
+        }
+    else{req.session.score=0;
+    	req.session.questions=[-1];}
 
     res.render('quizzes/randomresult', {
-        score: req.session.score,
+        score: score,
         result: result,
         answer: answer
     });
