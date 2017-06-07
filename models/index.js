45eddc1061d9863a12774e52bb9f5b39d921cc11
root <root@DESKTOP-5QJ609J.localdomain>
2017-06-07 10:55:40 +0200

    a ver

--- models/index.js
+++ models/index.js
@@ -25,6 +25,15 @@ var sequelize = new Sequelize(url, {storage: storage});
 
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+sequelize.sync()
+.then(function(){
+	console.log();
+})
+.catch(function(error){
+	console.log();
+	process.exit(1);
+	
+});
 
 
 // Importar la definicion de la tabla Tips de tips.js
